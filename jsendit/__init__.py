7705e9fe137f805b251467346632936e3845d19f
Pat Daburu <patdaburu@gmail.com>
2019-09-18 14:53:24 -0500

    Updated tag line.

--- jsendit/__init__.py
+++ jsendit/__init__.py
@@ -5,7 +5,8 @@
 .. currentmodule:: jsendit
 .. moduleauthor:: Pat Daburu <pat@daburu.net>
 
-This is a way simple library to help you format JSend messages.
+This is a way simple library to help you format
+`JSend <https://github.com/omniti-labs/jsend>`_ responses in your API.
 """
 from .respond import response, JSendStatus
 from .version import __version__, __release__
